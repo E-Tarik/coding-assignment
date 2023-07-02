e4f258c4b86ca6373207fcf15f82f14c616fd0ce
Igor Alekseev <ais_93@mail.ru>
2023-07-02 15:53:06 +0400

    Refactor: Use axios as http client

--- src/api/movies-api.js
+++ src/api/movies-api.js
@@ -1,18 +1,22 @@
+import axios from 'axios';
+
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
-const http = async url => {
-  const response = await fetch(`${ENDPOINT}${url}`);
+const http = axios.create({ baseURL: ENDPOINT, params: { api_key: API_KEY } });
 
-  return response.json();
+const request = async (url, { params = {} } = {}) => {
+  const response = await http(url, { params });
+
+  return response.data;
 };
 
 const api = {
-  discover: ({ sortBy = 'vote_count.desc' } = {}) =>
-    http(`/discover/movie?api_key=${API_KEY}&sort_by=${sortBy}`),
-  search: ({ query = '' } = {}) => http(`/search/movie?api_key=${API_KEY}&query=${query}`),
+  discover: async ({ sortBy = 'vote_count.desc' } = {}) =>
+    request('/discover/movie', { params: { sort_by: sortBy } }),
+  search: async ({ query = '' } = {}) => request('/search/movie', { params: { query } }),
   movieDetails: ({ movieId, appendToResponse = 'videos' }) =>
-    http(`/movie/${movieId}?api_key=${API_KEY}&{append_to_response=${appendToResponse}`),
+    request(`/movie/${movieId}`, { params: { append_to_response: appendToResponse } }),
 };
 
 export { api };
