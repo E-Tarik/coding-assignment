2ee5b0379e3645caaf5a3e7e1f78959306ded7c6
Igor Alekseev <ais_93@mail.ru>
2023-07-02 21:56:54 +0400

    Refactor: Use Bootstrap from NPM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './data/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.scss';
 import App from './App';
 
